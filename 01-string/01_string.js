302de723b44f1851dd37c65a023cec5718fb68aa
raj-chauhan212 <rajchauhan200212@gmail.com>
2024-03-18 09:49:02 +0530

    done

---
+++ 01-string/01_string.js
@@ -0,0 +1,56 @@
+// ## 0101-Print a String - Write a program which print below outout when program run/executed.
+// console.log("Rudra IT hub")
+
+// # 0102-length of a String - Write a program to find a lenght of a string and disply lenthg when program run/executed. let text ="welcome to IT hub";
+// let length = text.length;
+// console.log(length)
+
+
+
+// ## 0103-join a string - Write a program to join two string.
+// let name1 = "hello";
+// let name2 = "world";
+// let name3 =name1.concat(" ",name2);
+
+// console.log(name3)
+
+// ## 0104 - write a program print getting the first element of the String.
+// let text = "HELLO";
+// let char = text.charAt(0); 
+// console.log(char);
+
+// # 0105 - write a program separate individual characters from a string.// let element = "Rudra It, hub.com";
+// console.log(element.split(''));
+
+// # 0106 - write a program to print individual characters of a string in reverse order.
+// let element = "Rudra";
+// let reversedString =
+//     console.log(element.split('').reverse().join(""))
+
+// ## 0107 - write a program to count the total number of words in a string.
+// let word = "This is rudra IT hub";
+// let words = word.split(" ").length;
+// console.log(words)
+
+
+// ## 0108 - write a program to to convert a string to lowercase.
+
+// let str1 = "WELCOME TO RUDRA IT HUB";
+// let text = str1.toLowerCase();   
+// console.log(text)
+
+// ## 0109 - write a program to replace a specific word or character in a text string with another word or character.
+// let str1 = "rudra ";
+// str1 = str1.replace("rudra","visual");
+// console.log(str1)
+
+// # 0110 - Create a program that extracts a substring from a string based on the starting and ending positions provided.
+// let office = "rudra";
+// let result = office.substring(0,3);
+// console.log (result)
+
+// # 0111 - Write a program that removes leading and trailing whitespace from a given string.
+// let text1 = "          rudra        ";
+// text1= text1.trim("rudra");
+// console.log(text1)
+
