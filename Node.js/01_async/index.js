9009e9860c140ad601dbff262155837fd4102c28
raj-chauhan212 <rajchauhan200212@gmail.com>
2024-03-18 12:40:41 +0530

    done

--- Node.js/01_async/index.js
+++ Node.js/01_async/index.js
@@ -1,32 +1,31 @@
-// ex -1 
 const { Console } = require("console");
 const fs = require("fs")
 
-// ex-2
+// ex-1
 
 // fs.writeFile("write.txt","I am right learn file module",(err)=>{
 //     console.log("write");
 // });
 
-// ex-3
+// ex-2
 
 // fs.appendFile("write.txt","  I AM APPEND DATA IN WRITE FILE",(err)=>{
 //     console.log("creat file ");
 // })
 
-// ex-4
+// ex-3
 
 // fs.readFile("write.txt",'utf-8',(err,data)=>{
 //     console.log(err);
 // })
 
-// ex-5
+// ex-4
 
 // fs.unlink("write.txt",(err)=>{
 //     console.log(err);
 // })
 
-// ex-6
+// ex-5
 
 fs.rmdir("./npmMod", (err) => {
     console.log("folder deleted");
